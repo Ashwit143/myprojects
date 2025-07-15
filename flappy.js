e67c49ef07d6029464a9821f8ea395517fe38a69
Ashwit143 <uduthalaashwit@gmail.com>
2025-07-15 11:39:31 +0530

    Delete flappy.js

--- flappy.js
+++
@@ -1,9 +0,0 @@
-let flappybird = document.querySelector(".flappybird");
-let jump = document.querySelector(".jumpbtn");
-let hide = document.querySelector(".hide");
-
-jump.addEventListener("click", Jump());
-
-Jump = () => {
-    flappybird.style.top ="100px";
-}
